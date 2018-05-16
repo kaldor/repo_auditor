1455412cabd027b4e401394c566c0341c24b9a36
Nicholas Helke <nicholas.helke@kaldorgroup.com>
2018-05-16 17:26:08 +0100

    Skip already cached repos (unless `-f`), gzip archives for better space utilisation, and provide better error message on fatal error while archiving

--- fetch_repo_archives.sh
+++ fetch_repo_archives.sh
@@ -3,15 +3,39 @@
 set -e -u
 
 branch=master
-while getopts "c:b:" arg; do
+force=false
+while getopts "c:b:f" arg; do
   case "$arg" in
     c) cache=$OPTARG;;
     b) branch=$OPTARG;;
+    f) force=true;;
   esac
 done
 shift $((OPTIND-1))
 
+fetch_archive() {
+  repo=$1
+  dest=$2
+
+  tmp=$(mktemp -dt audit_archive_XXXXXX)
+  trap 'rm -rf "$tmp"' RETURN
+  trap 'echo Fatal error while fetching $repo' EXIT
+  git archive --format=tar --remote="$repo" -o "$tmp/archive.tar" "$branch"
+  gzip "$tmp/archive.tar"
+  mv "$tmp/archive.tar.gz" "$dest"
+  trap - EXIT
+  return 0
+}
+
 for repo in "$@"; do
-  archive=$(mktemp -t audit_archive_XXXXXX.tar)
-  git archive --format=tar --remote="$repo" -o "$archive" "$branch" && mv "$archive" "$cache/$(basename "$repo" .git).tar" || (rm "$archive"; echo "fatal: $repo"  >&2)
+  dest=$cache/$(basename "$repo" .git).tgz
+  if [[ -f "$dest" ]] && ! $force; then
+    echo "Skipping $repo, $dest found locally, use -f to overwrite"
+    continue
+  fi
+  if ! git ls-remote --exit-code "$repo" > /dev/null; then
+    echo "Skipping $repo, could not fetch $branch"
+    continue
+  fi
+  fetch_archive "$repo" "$dest"
 done
